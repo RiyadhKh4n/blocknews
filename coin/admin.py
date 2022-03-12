5ab7b302edd9b64686b93807b0c12b84129e7511
Arkay <riyadh.k@outlook.com>
2022-03-12 15:24:08 +0000

    Add list_filter and list_display to Coin

--- coin/admin.py
+++ coin/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from coin.models import Coin
 
-
-admin.site.register(Coin)
+@admin.register(Coin)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_filter = ('user', 'ticker')
+    list_display = ('user', 'ticker')
+    search_fields = ['ticker']
