a26ea2e05cc6c3761ae401ad44b2286ce47cf54a
Arkay <riyadh.k@outlook.com>
2022-03-15 18:20:42 +0000

    Add portfolioID to exclude

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -3,18 +3,14 @@ from .models import Portfolio, Asset
 
 
 class PortfolioForm(forms.ModelForm):
-    # user = forms.CharField(initial={'user': 'User'})
-
     class Meta:
         model = Portfolio
         exclude = ['user', 'slug', 'USDvalue']
         fields = ['name']
 
+
 class AddAsset(forms.ModelForm):
     class Meta:
-        models = Asset
-        fields = ['CoinID', 'quantity', 'AveragePrice', 'USDSpent', 'USDEarned', 'CurrentInvestment', 'PnL']
-
-
-
-        
+        model = Asset
+        exclude = ['portfolioID']
+        fields = ['coinID', 'quantity', 'AveragePrice', 'USDSpent', 'USDEarned', 'CurrentInvestment', 'PnL']
