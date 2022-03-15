abd78353ab150827f7010c3508502b37660b7c34
Arkay <riyadh.k@outlook.com>
2022-03-15 15:35:48 +0000

    Change PortfolioForm so only field is name

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -3,13 +3,18 @@ from .models import Portfolio, Asset
 
 
 class PortfolioForm(forms.ModelForm):
+    # user = forms.CharField(initial={'user': 'User'})
+
     class Meta:
         model = Portfolio
-        fields = ['name', 'USDvalue', 'user', 'slug']
-        # fields = ['name']
-
+        exclude = ['user', 'slug', 'USDvalue']
+        fields = ['name']
 
 class AddAsset(forms.ModelForm):
     class Meta:
         models = Asset
         fields = ['CoinID', 'quantity', 'AveragePrice', 'USDSpent', 'USDEarned', 'CurrentInvestment', 'PnL']
+
+
+
+        
