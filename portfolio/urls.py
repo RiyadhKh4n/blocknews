62dad353e80fd0c679ffd3fa671bcec2294ec5e4
Arkay <riyadh.k@outlook.com>
2022-03-15 16:59:49 +0000

    Change url for get_portfolio_list

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.get_portfolio_list, name='get_portfolio_list'),
-    # path('<user_id>', views.get_portfolio_list_1, name='get_portfolio_list_1'),
     path('edit/<portfolio_id>', views.edit_portfolio, name='edit_portfolio'),
     path('delete/<portfolio_id>', views.delete_portfolio, name='delete_portfolio'),
     path('create', views.create_portfolio, name='create_portfolio_url'),
