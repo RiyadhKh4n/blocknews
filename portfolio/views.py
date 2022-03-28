9ed4bb70ee25a550018ee131903826cbedbc9aa5
Arkay <riyadh.k@outlook.com>
2022-03-28 21:20:10 +0000

    Update add_asset() to prevent duplicate portfolio entries

--- portfolio/views.py
+++ portfolio/views.py
@@ -64,20 +64,31 @@ def add_asset(request, portfolio_id, coin_id=None):
     if request.method == "POST":
         form = AddAsset(request.POST)
         if form.is_valid():
-            obj = form.save(commit=False)
-            obj.PnL = 0.00
-            obj.USDEarned = 0.00
-            obj.portfolio_name = portfolio
-            quantity = form['quantity'].value()
-            AP = form['AveragePrice'].value()
-            obj.AveragePrice = AP
-            # coinID = form['coinID'].value()
-            # price = get_coin_price(coinID)
-            USDspent = (int(quantity) * float(AP))
-            obj.USDSpent = USDspent
-            obj.CurrentInvestment = USDspent
-            obj.save()
-            return redirect(reverse('get_asset_list', args=[portfolio_id]))
+            coin = form['coinID'].value()
+            try:
+                asset = Asset.objects.get(portfolio_name=portfolio_id, coinID=coin)
+                print(asset)
+                print("A Coin with the ID " + coin + " exists!")
+                pk = asset.id
+                b_or_s = 'buy'
+                update_asset(request, pk, b_or_s)
+
+            except Asset.DoesNotExist:
+                print("Coin with that ID doesn't exist")
+                obj = form.save(commit=False)
+                obj.PnL = 0.00
+                obj.USDEarned = 0.00
+                obj.portfolio_name = portfolio
+                obj.AveragePrice = form['AveragePrice'].value()
+                quantity = float(form['quantity'].value())
+                AP = form['AveragePrice'].value()
+                # coinID = form['coinID'].value()
+                # price = get_coin_price(coinID)
+                USDspent = (quantity) * float(AP)
+                obj.USDSpent = USDspent
+                obj.CurrentInvestment = USDspent
+                obj.save()
+                return redirect(reverse('get_asset_list', args=[portfolio_id]))
 
     context = {'form': form}
     return render(request, 'portfolio/add_asset.html', context)
@@ -88,12 +99,18 @@ def update_asset(request, pk, b_or_s):
     if request.method == 'POST':
         form = UpdateAsset(request.POST, instance=asset)
         asset_qty = float(asset.quantity)
+        curr_inv = float(asset.CurrentInvestment)
+        curr_usd_earned = float(asset.USDEarned)
         if b_or_s == 'buy':
             # do the calculations for BUYING
             new_qty = float(form['quantity'].value())
+            new_inv = float(form['AveragePrice'].value()) * new_qty
             asset.quantity = asset_qty + new_qty
+            asset.CurrentInvestment = curr_inv + new_inv
+            asset.USDSpent = asset.CurrentInvestment
             asset.save()
             messages.success(request, f"{new_qty} {asset} successfully purchased!")
+
         elif b_or_s == 'sell':
             # do the calculations for SELLING
             if float(form['quantity'].value()) > asset_qty:
@@ -101,7 +118,10 @@ def update_asset(request, pk, b_or_s):
                 return redirect(update_asset, pk, 'sell')
             else:
                 new_qty = float(form['quantity'].value())
+                price = float(form['AveragePrice'].value())
+                usd_earned = price * new_qty
                 asset.quantity = asset_qty - new_qty
+                asset.USDEarned = curr_usd_earned + usd_earned
                 if asset.quantity <= 0:
                     asset.delete()
                 else:
