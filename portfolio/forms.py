28f17c3de757a365cce5c7b153ae4a078a549a10
Arkay <riyadh.k@outlook.com>
2022-03-29 19:22:29 +0000

    Change fields to match with model

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -5,18 +5,18 @@ from .models import Portfolio, Asset
 class PortfolioForm(forms.ModelForm):
     class Meta:
         model = Portfolio
-        exclude = ['user', 'slug', 'USDvalue']
+        exclude = ['user', 'slug', 'usd_value']
         fields = ['name']
 
 
 class AddAsset(forms.ModelForm):
     class Meta:
         model = Asset
-        exclude = ['portfolio_name', 'PnL', 'USDEarned', 'USDSpent', 'CurrentInvestment']
-        fields = ['coinID', 'quantity', 'AveragePrice']
+        exclude = ['portfolio_name', 'pnl', 'usd_earned', 'usd_spent', 'current_investment']
+        fields = ['coinID', 'quantity', 'average_price']
 
 
 class UpdateAsset(forms.ModelForm):
     class Meta:
         model = Asset
-        fields = ['quantity', 'AveragePrice']
+        fields = ['quantity', 'average_price']
