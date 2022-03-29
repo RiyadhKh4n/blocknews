5d7b49f754bad5d777e317ecae857fea70e6c0ba
Arkay <riyadh.k@outlook.com>
2022-03-29 19:22:13 +0000

    Change fields to match with model

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -6,15 +6,15 @@ from portfolio.models import Portfolio, Asset
 class PortfolioAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_filter = ('user', 'name', 'created_on')
-    list_display = ('name', 'user', 'created_on', 'USDvalue')
+    list_display = ('name', 'user', 'created_on', 'usd_value')
     search_fields = ['name', 'user']
 
 
 @admin.register(Asset)
 class AssetAdmin(admin.ModelAdmin):
-    list_display = ('portfolio_user', 'coinID', 'portfolio_name', 'PnL', 'quantity', 'added_to_portfolio')
+    list_display = ('portfolio_user', 'coinID', 'portfolio_name', 'pnl', 'quantity', 'added_to_portfolio')
     search_fields = ['coinID', 'portfolio_name']
-    list_filter = ('coinID', 'portfolio_name', 'PnL', 'added_to_portfolio')
+    list_filter = ('coinID', 'portfolio_name', 'pnl', 'added_to_portfolio')
 
     def portfolio_user(self, instance):
         return instance.portfolio_name.user
