7ad25a2473bd696cb4b5a3ec7fd0a30c1d56866d
Arkay <riyadh.k@outlook.com>
2022-03-30 18:14:17 +0000

    Remove coin APP and all files with, migrate changes

--- coin/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from coin.models import Coin
-
-@admin.register(Coin)
-class PortfolioAdmin(admin.ModelAdmin):
-    list_filter = ('user', 'ticker')
-    list_display = ('user', 'ticker')
-    search_fields = ['ticker']
