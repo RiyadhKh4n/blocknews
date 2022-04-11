9f33da6a1ebf74dd82d2849266d72c9d5268d5c3
Arkay <riyadh.k@outlook.com>
2022-04-11 16:55:08 +0000

    Remove exlude list and comment

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -5,14 +5,12 @@ from .models import Portfolio, Asset
 class PortfolioForm(forms.ModelForm):
     class Meta:
         model = Portfolio
-        exclude = ['user', 'slug', 'usd_value']
         fields = ['name']
 
 
 class AddAsset(forms.ModelForm):
     class Meta:
         model = Asset
-        # exclude = ['portfolio_name', 'pnl', 'usd_earned', 'usd_spent', 'current_investment']
         fields = ['quantity', 'price', 'average_price']
 
 
