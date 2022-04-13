73533ace96b32f50fe2f9f2b1a49e42ae9280678
Arkay <riyadh.k@outlook.com>
2022-04-13 19:24:28 +0000

    Update views to allow for multi-step form and add validate_ticker()

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib import messages
-# from formtools.wizard.views import SessionWizardView
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 from .models import Portfolio, Asset
@@ -52,11 +51,28 @@ def get_ticker_list(data):
 
 
 def get_coin_price(ticker, coins):
+    """
+    Will get the price of a coin
+    """
     if ticker in tickerList:
         for x in coins:
             if x['symbol'] == ticker:
                 price = float((x['quote']['USD']['price']))
         return price
+
+
+def validate_ticker(ticker):
+    """
+    Will validate if the users ticker exits in tickerList
+    """
+    for x in tickerList:
+        if ticker in tickerList:
+            print("Valid ticker")
+            return True
+
+        else:
+            print("Invalid ticker")
+            return False
 
 
 def get_portfolio_list(request):
@@ -111,55 +127,80 @@ def get_asset_list(request, portfolio_id):
     return render(request, 'portfolio/assets.html', context)
 
 
-def add_asset(request, portfolio_id, coin_id=None):
+def get_asset(request, portfolio_id):
     portfolio = Portfolio.objects.get(pk=portfolio_id)
     returnedCoin = call_api()
-    form = AddAsset()
+
     if request.method == "POST":
         coin = request.POST.get("ticker")
+        valid = validate_ticker(coin)
+
+        if valid:
+            price = get_coin_price(coin, returnedCoin)
+            return redirect(add_asset, portfolio.id, coin, price)
+
+        else:
+            messages.error(request, f"{coin} does not exist!")
+
+    context = {
+        'tickerList': tickerList,
+        'portfolio': portfolio,
+    }
+    return render(request, 'portfolio/get_ticker.html', context)
+
+
+def add_asset(request, portfolio, coin, price):
+    portfolio = Portfolio.objects.get(pk=portfolio)
+    form = AddAsset()
+    if request.method == "POST":
         form = AddAsset(request.POST)
         if form.is_valid():
             try:
-                asset = Asset.objects.get(portfolio_name=portfolio_id, ticker=coin)
-                print(asset)
+                asset = Asset.objects.get(portfolio_name=portfolio, ticker=coin)
                 pk = asset.id
                 b_or_s = 'buy'
-                update_asset(request, pk, b_or_s)
-
-                return redirect(reverse('get_asset_list', args=[portfolio_id]))
+                update_asset(request, pk, b_or_s, coin, price)
+                return redirect(reverse('get_asset_list', args=[portfolio.id]))
 
             except Asset.DoesNotExist:
                 obj = form.save(commit=False)
                 quantity = float(form['quantity'].value())
-                AP = float(form['average_price'].value())
-                ticker_price = get_coin_price(coin, returnedCoin)
-                obj.price = ticker_price
-                print(ticker_price)
-                USDspent = quantity * ticker_price
+                AP = float(request.POST.get('average_price'))
+                obj.price = price
+                USDspent = float(quantity) * float(price)
                 obj.average_price = AP
                 obj.portfolio_name = portfolio
                 obj.ticker = coin
                 obj.pnl = 0.00
                 obj.usd_earned = 0.00
                 obj.usd_spent = USDspent
-                obj.current_holdings = quantity * ticker_price
+                obj.current_holdings = float(quantity) * float(price)
                 obj.save()
-                return redirect(reverse('get_asset_list', args=[portfolio_id]))
+                return redirect(reverse('get_asset_list', args=[portfolio.id]))
 
     context = {
         'form': form,
-        'tickerList': tickerList
+        'tickerList': tickerList,
+        'ticker': coin,
+        'price': price,
     }
     return render(request, 'portfolio/add_asset.html', context)
 
 
-def update_asset(request, pk, b_or_s):
+def update_asset(request, pk, b_or_s, coin, price):
     asset = get_object_or_404(Asset, pk=pk)
     returnedCoin = call_api()
     if request.method == 'POST':
-        coin = request.POST.get("ticker")
-        ticker_price = get_coin_price(coin, returnedCoin)
-        print(ticker_price)
+        if coin is not None:
+            coin = coin
+        else:
+            coin = request.POST.get("ticker")
+        if price is not None:
+            price = get_coin_price(coin, returnedCoin)
+        else:
+            price = price
+        # coin = coin if coin is not None else request.POST.get("ticker")  # ternary operator not working
+        # price = price if price is not None else get_coin_price(coin, returnedCoin)  # ternary operator not working
         form = UpdateAsset(request.POST, instance=asset)
         asset_qty = float(asset.quantity)
         curr_spent = float(asset.usd_spent)
@@ -168,10 +209,11 @@ def update_asset(request, pk, b_or_s):
         if b_or_s == 'buy':
             # do the calculations for BUYING
             new_qty = float(form['quantity'].value())
-            new_inv = ticker_price * new_qty
+            new_inv = float(price) * float(new_qty)
+            asset.price = price
             asset.quantity = asset_qty + new_qty
             asset.usd_spent = curr_spent + new_inv
-            asset.current_holdings = (asset.quantity) * ticker_price
+            asset.current_holdings = float(asset.quantity) * float(price)
             asset.ticker = coin
             asset.save()
             messages.success(request, f"{new_qty} {coin} successfully purchased!")
@@ -188,7 +230,7 @@ def update_asset(request, pk, b_or_s):
                 asset.ticker = coin
                 asset.quantity = asset_qty - new_qty
                 asset.usd_earned = curr_usd_earned + usd_earned
-                asset.current_holdings = (asset.quantity) * ticker_price
+                asset.current_holdings = (asset.quantity) * price
                 if asset.quantity <= 0:
                     asset.delete()
                 else:
@@ -207,20 +249,3 @@ def update_asset(request, pk, b_or_s):
         'form': form,
     }
     return render(request, 'portfolio/buy_sell_asset.html', context)
-
-
-# class FormWizardView(SessionWizardView):
-#     template_name = "portfolio/add_asset_form.html"
-#     form_list = [ContactForm1, ContactForm2]
-
-#     def done(self, form_list, **kwargs):
-#         return render(self.request, 'done.html', {
-#             'form_data': [form.cleaned_data for form in form_list],
-#         })
-
-
-# def process_form_data(form_list):
-#     form_data = [form.cleaned_Data for form in form_list]
-
-#     logr.debug(form_data[0]['ticker'])
-#     logr.debug(form_data[1]['ticker', 'quantity', 'price'])
