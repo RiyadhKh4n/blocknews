c9db106473e56ce8176e2cc023df88075a5fa3ad
Arkay <riyadh.k@outlook.com>
2022-04-27 16:57:02 +0000

    Add docstring to PortfolioConfig() class

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class PortfolioConfig(AppConfig):
+    """
+    Class that configures the Portfolio
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio'
