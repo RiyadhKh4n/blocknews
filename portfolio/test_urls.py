a93766eb7bc1c4ad5a04e55f903a1fc5299503df
Arkay <riyadh.k@outlook.com>
2022-04-28 14:13:09 +0000

    Add tests for urls

--- portfolio/test_urls.py
+++ portfolio/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from .models import Portfolio
-from .views import get_portfolio_list, edit_portfolio, delete_portfolio, create_portfolio, get_asset_list, add_asset, update_asset
+from .views import get_portfolio_list, edit_portfolio, delete_portfolio, create_portfolio, get_asset, get_asset_list, add_asset, update_asset  # noqa
 
 
 class TestPortfolioUrls(TestCase):
@@ -9,11 +9,30 @@ class TestPortfolioUrls(TestCase):
         url = reverse('get_portfolio_list')
         self.assertEquals(resolve(url).func, get_portfolio_list)
 
-    # def test_editPortfolio_url_is_resolved(self):
-    #     portfolio = Portfolio.objects.create(name='Django')
-    #     url = reverse(f'edit_portfolio/{portfolio.id}')
-    #     self.assertEquals(resolve(url).func, edit_portfolio)
+    def test_editPortfolio_url_is_resolved(self):
+        url = reverse('edit_portfolio', args=[1])
+        self.assertEqual(resolve(url).func, edit_portfolio)
 
-    # def test_createPortfolio_url_is_resolved(self):
-    #     url = reverse('create_portfolio')
-    #     self.assertEquals(resolve(url).func, create_portfolio)
+    def test_deletePortfolio_url_is_resolved(self):
+        url = reverse('delete_portfolio', args=[1])
+        self.assertEqual(resolve(url).func, delete_portfolio)
+
+    def test_createPortfolio_url_is_resolved(self):
+        url = reverse('create_portfolio_url')
+        self.assertEquals(resolve(url).func, create_portfolio)
+
+    def test_viewPortfolio_url_is_resloved(self):
+        url = reverse('get_asset_list', args=[1])
+        self.assertEquals(resolve(url).func, get_asset_list)
+
+    def test_get_url_is_resolved(self):
+        url = reverse('get_asset', args=[1])
+        self.assertEquals(resolve(url).func, get_asset)
+
+    def test_add_url_is_resolved(self):
+        url = reverse('add_asset_form', args=[1, 'ADA', '10.00'])
+        self.assertAlmostEquals(resolve(url).func, add_asset)
+
+    def test_update_url_is_resolved(self):
+        url = reverse('update_asset', args=[1, 'sell', 'ADA', '0.89'])
+        self.assertAlmostEquals(resolve(url).func, update_asset)
