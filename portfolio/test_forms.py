1e76ab365418a1733a617b73a49343a6fefbfd9d
Arkay <riyadh.k@outlook.com>
2022-04-28 14:13:40 +0000

    Complete test_forms.py

--- portfolio/test_forms.py
+++ portfolio/test_forms.py
@@ -13,3 +13,29 @@ class TestPortfolioForm(TestCase):
     def test_field_are_explicit_in_form_metaclass(self):
         form = PortfolioForm()
         self.assertEqual(form.Meta.fields, ['name'])
+
+
+class TestAddAssetForm(TestCase):
+
+    def test_portfolio_name_is_required(self):
+        form = AddAsset({'quantity': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('quantity', form.errors.keys())
+        self.assertEqual(form.errors['quantity'][0], 'This field is required.')
+
+    def test_field_are_explicit_in_form_metaclass(self):
+        form = AddAsset()
+        self.assertEqual(form.Meta.fields, ['quantity'])
+
+
+class TestUpdateAssetForm(TestCase):
+
+    def test_portfolio_name_is_required(self):
+        form = UpdateAsset({'quantity': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('quantity', form.errors.keys())
+        self.assertEqual(form.errors['quantity'][0], 'This field is required.')
+
+    def test_field_are_explicit_in_form_metaclass(self):
+        form = UpdateAsset()
+        self.assertEqual(form.Meta.fields, ['quantity'])
